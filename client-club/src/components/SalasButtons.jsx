8194e9fc2cd3b32e3ffd2411f1e2cb15fff68ba3
JorgeAzuaje <jorgeantonioazuajevilla@gmail.com>
2023-11-27 02:32:44 -0400

    Cree fromulario para reservar

--- client-club/src/components/SalasButtons.jsx
+++ client-club/src/components/SalasButtons.jsx
@@ -6,10 +6,10 @@ export default function SalasButtons() {
             <p className="text-black font-light text-3xl text-center pb-5 mb:text-4xl dark:text-white">Editar o eliminar esta sala</p>
             <div className="flex justify-center">
                 <div className='pt-5 grid justify-items-center mb-4 pr-1 md:pr-0 md:w-1/6'>
-                    <button type="" className='font-light bg-yellow-800 w-32 h-10 text-white text-2xl'>Editar</button>
+                    <button type="" className='font-light bg-[rgba(95,111,82,1)] w-32 h-10 text-white text-2xl'>Editar</button>
                 </div>
                 <div className='pt-5 grid justify-items-center mb-4 pl-1 md:pl-0 md:w-1/6'>
-                    <button type="" className='font-light bg-yellow-800 w-32 h-10 text-white text-2xl'>Eliminar</button>
+                    <button type="" className='font-light bg-[rgba(95,111,82,1)] w-32 h-10 text-white text-2xl'>Eliminar</button>
                 </div>
             </div>
             <style>
