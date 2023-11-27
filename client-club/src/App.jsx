39e6cfd29ee60847fd3c9885ad095ae2425f7063
JorgeAzuaje <jorgeantonioazuajevilla@gmail.com>
2023-11-27 01:01:46 -0400

    cree el landing

--- client-club/src/App.jsx
+++ client-club/src/App.jsx
@@ -1,12 +1,4 @@
-import Deportes from "./components/Deportes"
-import Eventos from "./components/Eventos"
-import Footer from "./components/Footer"
-import Home from "./components/Home"
-import Info from "./components/Info"
 import Login from "./components/Login"
-import NavBar from "./components/NavBar"
-import Restaurantes from "./components/Restaurantes"
-import Salas from "./components/Salas"
 import SalasView from "./components/SalasView"
 
 
@@ -15,16 +7,8 @@ function App() {
 
   return (
     <div>
-      {/* <Footer/> */}
-      {/* <Deportes/> */}
-      {/* <Salas/> */}
-      {/* <Restaurantes/> */}
-      {/* <Info/> */}
       {/* <SalasView/> */}
-      {/* <Home/> */}
-      {/* <NavBar/> */}
-      {/* <Eventos/> */}
-      <Login/>
+      {/* <Login/> */}
     </div>
     
   )
