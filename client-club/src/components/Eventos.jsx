9b3b9554735c11b2fbaf62e243cba9a3b83663fc
luis-sant1 <santiagomuchacholuma@uvm.edu.ve>
2023-12-03 21:52:31 -0600

    Update Eventos.jsx

--- client-club/src/components/Eventos.jsx
+++ client-club/src/components/Eventos.jsx
@@ -7,7 +7,6 @@ export default function Eventos() {
             <h1 className='font-bold pl-14 text-[rgba(95,111,82,1)] text-4xl md:text-7xl md:pl-16 md:pb-0 lg:text-8xl lg:pl-20'>Eventos</h1>
             <div className='flex w-full h-5/6 bg-[rgba(230,230,230,1)] pt-10 pb-5 lg:pb-10' >
                 <div className='w-11/12 overflow-hidden m-auto'>
-
                     <CarruselEventos />
                 </div>
             </div>
