56eaab1f240fd51d8da55a5537a2d02c1dc90ed8
OmarDavidHernandez <davidsalcedo1994@gmail.com>
2023-12-04 04:15:32 -0400

    Corrección de Información en Acerda de Nosotros

--- client-club/src/components/Info.jsx
+++ client-club/src/components/Info.jsx
@@ -37,24 +37,23 @@ export default function Info() {
                     transition={{ duration: 1, delay: 0.5}}>
                     <div ref={ref} className="pt-12 md:flex ">
                         <div className="border-b border-bottom-width: 1px; border-black border-solid pl-5 md:w-1/2 md:border-b-0 md:border-r md:pt-10 md:pr-10 lg:pl-14">
-                            <p className="text-lg font-light pb-5 lg:text-xl">Somos un hotel con una valoración de 5 estrellas ubicado en Tucacas estado Falcón, es pacíficamente en Mar Leon, Tucacas 2055, Falcón, en nuestra ciudad podrás disfrutar de las mejores playas de Venezuela y estamos seguros que podemos asegurar también que son de las mejores del mundo entero. </p>
-                            <p className="text-lg font-light pb-7 lg:text-xl">Contamos con 5 tipos de habitaciones distintas que van desde habitaciones matrimoniales, hasta habitaciones para 6 personas, contamos con restaurante, piscina y barras de bebidas, para que tengas una experiencia inolvidable en nuestras instalaciones de Perubiam Hotel Suites, donde tu comodidad y confort es nuestra prioridad.</p>
+                            <p className="text-lg font-light pb-5 lg:text-xl">Somos un Club Campestre con casi un Siglo de historia, siempre brindando la mayor calidad de servicios a nuestros socios para que disfruten de emocionantes o relajantes días junto a sus seres queridos. Estamos orgullosos de nuestra historia y esperamos que usted se convierta en parte de esta. </p>
+                            <p className="text-lg font-light pb-7 lg:text-xl">Contamos con los mejores servicios del país en lo que respecta a Canchas para tus deportes favoritos, aunque nuestro orgullo no viene sólo de nuestros servicios en los deportes, sino también en los increíbles eventos que organizamos para que nuestros socios pasen momentos inolvidables sin importar la situación. Además, contamos con restaurantes de la mayor calidad a través del Campo entero, por lo que podrás pasar un Almuerzo o Cena espectacular en nuestras instalaciones. </p>
                         </div>
                         <div className="pt-7 md:w-1/2 md:pt-10 md:pl-10 lg:pl-14">
                             <h1 className="text-3xl font-light pb-5 pl-5 lg:text-4xl">Horarios</h1>
                             <ul className="list-disc pl-16">
-                                <li className="font-light text-lg lg:text-xl">Check-in: 1PM</li>
-                                <li className="font-light text-lg lg:text-xl">Check-out: 12PM</li>
-                                <li className="font-light text-lg lg:text-xl">Piscina: 10AM-10PM</li>
-                                <li className="font-light text-lg lg:text-xl">Restaurant: 8AM-11PM</li>
-                                <li className="font-light text-lg lg:text-xl">Barra de bebidas: 8AM-11PM</li>
+                                <li className="font-light text-lg lg:text-xl">Abrimos a las: 7PM</li>
+                                <li className="font-light text-lg lg:text-xl">Cerramos a las: 10PM</li>
+                                <li className="font-light text-lg lg:text-xl">Piscina: 9AM-9PM</li>
+                                <li className="font-light text-lg lg:text-xl">Restaurants: 8AM-10PM</li>
                             </ul>
                             <h1 className="text-3xl font-light pb-5 pt-5 pl-5 lg:text-4xl">Contáctanos</h1>
                             <ul className="list-disc pl-16">
-                                <li className="font-light text-lg lg:text-xl">Número de teléfono: 0418-4567-890</li>
-                                <li className="font-light text-lg lg:text-xl">Instagram: @prubiam1_hotelsuits</li>
-                                <li className="font-light text-lg lg:text-xl">Correo electrónico: perubiamhotelsuits@gmail.com</li>
-                                <li className="font-light text-lg lg:text-xl">Subreddit: /perubiam1</li>
+                                <li className="font-light text-lg lg:text-xl">Número de teléfono: 0418-5667-670</li>
+                                <li className="font-light text-lg lg:text-xl">Instagram: @club1_canaima</li>
+                                <li className="font-light text-lg lg:text-xl">Correo electrónico: clubcampestrecanaima@gmail.com</li>
+                                <li className="font-light text-lg lg:text-xl">Subreddit: /CampestreCanaima</li>
                             </ul>
                         </div>
                     </div>
